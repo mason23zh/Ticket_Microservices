54f9c56edd7eb9db4e815436e3cc8c7419ae01d1
Zhengyang Zhao <mason.zh@gmail.com>
2022-01-06 05:20:02 -0700

    change config to build in local

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import "express-async-errors"; //handle express errors, no needs to pass the new Error into the next() function
 import { json } from "body-parser";
 
 import { currentUserRouter } from "./routes/current-user";
@@ -6,6 +7,7 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -15,8 +17,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log("Listening on port: 3000 (On Clould Mofo)");
+  console.log("Listening on port: 3000");
 });
