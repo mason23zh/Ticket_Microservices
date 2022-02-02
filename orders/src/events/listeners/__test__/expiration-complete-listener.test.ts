63d13d9cc5f7ddb629c77c1c9152ec1d7d5025e9
Zhengyang Zhao <mason.zh@gmail.com>
2022-02-02 05:06:36 -0600

    added expiration-complete-listener in order server and test

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,71 @@
+import { Ticket } from "../../../models/ticket";
+import { natsWrapper } from "../../../nats-wrapper";
+import { ExpirationCompleteListener } from "../expiration-complete-listener";
+import mongoose from "mongoose";
+import { Order } from "../../../models/order";
+import { OrderStatus, ExpirationCompleteEvent } from "@ortick/new-common";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+    id: new mongoose.Types.ObjectId().toHexString(),
+  });
+
+  await ticket.save();
+
+  const order = Order.build({
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+    ticket,
+  });
+
+  await order.save();
+
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id,
+  };
+
+  //@ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, order, ticket, data, msg };
+};
+
+it("updates the order status to cancelled", async () => {
+  const { listener, order, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it("emit an OrderCancelled event", async () => {
+  const { listener, order, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const eventData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+
+  expect(eventData.id).toEqual(order.id);
+});
+
+it("ack the message", async () => {
+  const { listener, order, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
