ec8aec3ddd5326e77d751b8fcc46d41c384c11ec
Zhengyang Zhao <mason.zh@gmail.com>
2022-02-01 23:39:44 -0600

    initial setup for expiration server

---
+++ expiration/src/index.ts
@@ -0,0 +1,38 @@
+import { natsWrapper } from "./nats-wrapper";
+
+// Start up logic
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("ATS_CLIENT_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  try {
+    // Connect to Nats
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    // Graceful shutdown
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    // NOT working in Windows
+    process.on("SIGTERM", () => natsWrapper.client.close());
+    process.on("SIGINT", () => natsWrapper.client.close());
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
